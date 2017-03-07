1aae97bd2a24f2e16200149171b545a74ef00d1a
Cisco <fjdiaz2k11@hotmail.com>
2017-03-06 23:40:24 -0800

    Add base page

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import helpers.WebDriverLibrary;
+
+/**
+ * Created by Cisco on 3/6/2017.
+ */
+public class BasePage extends WebDriverLibrary {
+}
--- src/test/java/pages/sample_page.java
+++ src/test/java/pages/sample_page.java
@@ -1,11 +1,9 @@
 package pages;
 
-import helpers.WebDriverLibrary;
-
 /**
  * Created by Cisco on 3/6/2017.
  */
-public class sample_page extends WebDriverLibrary {
+public class sample_page extends BasePage {
 
 
 
